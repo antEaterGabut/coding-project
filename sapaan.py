6c49b47e905e3d712cdb9573fe311566a783d66e
antEaterGabut <newbienetworkcyber@gmail.com>
2026-05-30 19:46:58 +0700

    day 1 coding project

---
+++ sapaan.py
@@ -0,0 +1,15 @@
+print("namamu siapa?")
+
+nama = input("nama: ")
+
+print("halo", nama)
+
+print("namaku termux!")
+print("eh kamu kelas berapa?")
+
+kelas = float(input("kelas: "))
+
+if kelas < 12:
+	print("wah masih pelajar dong")
+else: 
+	print("semangat yah kalo udah mau lulus")
